62490506bca0c122e9c6585ce3e73ebf22d5e685
AM7754 <am7754@zebra.com>
2024-09-02 21:02:16 +0530

    Azure CLI login

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
   features = {}
   # Service Principal Authentication
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  client_id       = var.client_id != "" ? var.client_id : env.ARM_CLIENT_ID
+  client_secret   = var.client_secret != "" ? var.client_secret : env.ARM_CLIENT_SECRET
+  subscription_id = var.subscription_id != "" ? var.subscription_id : env.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.tenant_id != "" ? var.tenant_id : env.ARM_TENANT_ID
 }
 
 
