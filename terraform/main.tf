31814db82444f1a66e124f9eb1936f00acf47a70
AM7754 <am7754@zebra.com>
2024-09-03 08:29:19 +0530

    Azure terraform setup -2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features = {}
-  # Service Principal Authentication
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 
