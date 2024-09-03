e31d740af7d5def03a5d2097a25ffec972d904b8
AM7754 <am7754@zebra.com>
2024-09-03 08:31:07 +0530

    Azure terraform setup -3

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
-variable "client_id" {
-  description = "Azure Client ID"
-}
-
-variable "client_secret" {
-  description = "Azure Client Secret"
-  sensitive   = true
-}
-
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-}
+#variable "client_id" {
+#  description = "Azure Client ID"
+#}
+#
+#variable "client_secret" {
+#  description = "Azure Client Secret"
+#  sensitive   = true
+#}
+#
+#variable "subscription_id" {
+#  description = "Azure Subscription ID"
+#}
+#
+#variable "tenant_id" {
+#  description = "Azure Tenant ID"
+#}
